50a5e18aa30c5806cee8ec18a828ff94e986e7dc
Kirk Wight <kwight@kwight.ca>
2012-07-05 15:01:02 -0400

    Added a search form to the header

--- header.php
+++ header.php
@@ -51,6 +51,8 @@
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 		</hgroup>
 
+		<?php get_search_form(); ?>
+
 		<nav role="navigation" class="site-navigation main-navigation">
 			<h1 class="assistive-text"><?php _e( 'Menu', '_s' ); ?></h1>
 			<div class="assistive-text skip-link"><a href="#content" title="<?php esc_attr_e( 'Skip to content', '_s' ); ?>"><?php _e( 'Skip to content', '_s' ); ?></a></div>
