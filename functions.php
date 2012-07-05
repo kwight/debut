53fde2993f004d14fd324bc522122588fcc10301
Kirk Wight <kwight@kwight.ca>
2012-07-05 15:14:31 -0400

    Moved functions from included file to functions.php

--- functions.php
+++ functions.php
@@ -88,3 +88,100 @@ function _s_scripts() {
 	}
 }
 add_action( 'wp_enqueue_scripts', '_s_scripts' );
+
+/**
+ * Display navigation to next/previous pages when applicable
+ *
+ * @since _s 1.0
+ */
+if ( ! function_exists( '_s_content_nav' ) ):
+function _s_content_nav( $nav_id ) {
+	global $wp_query;
+
+	$nav_class = 'site-navigation paging-navigation';
+	if ( is_single() )
+		$nav_class = 'site-navigation post-navigation';
+
+	?>
+	<nav role="navigation" id="<?php echo $nav_id; ?>" class="<?php echo $nav_class; ?>">
+		<h1 class="assistive-text"><?php _e( 'Post navigation', '_s' ); ?></h1>
+
+	<?php if ( is_single() ) : // navigation links for single posts ?>
+
+		<?php previous_post_link( '<div class="nav-previous">%link</div>', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', '_s' ) . '</span> %title' ); ?>
+		<?php next_post_link( '<div class="nav-next">%link</div>', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', '_s' ) . '</span>' ); ?>
+
+	<?php elseif ( $wp_query->max_num_pages > 1 && ( is_home() || is_archive() || is_search() ) ) : // navigation links for home, archive, and search pages ?>
+
+		<?php if ( get_next_posts_link() ) : ?>
+		<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', '_s' ) ); ?></div>
+		<?php endif; ?>
+
+		<?php if ( get_previous_posts_link() ) : ?>
+		<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', '_s' ) ); ?></div>
+		<?php endif; ?>
+
+	<?php endif; ?>
+
+	</nav><!-- #<?php echo $nav_id; ?> -->
+	<?php
+}
+endif; // _s_content_nav
+
+/**
+ * Prints HTML with meta information for the current post-date/time and author.
+ *
+ * @since _s 1.0
+ */
+if ( ! function_exists( '_s_posted_on' ) ) :
+function _s_posted_on() {
+	printf( __( 'Posted on <a href="%1$s" title="%2$s" rel="bookmark"><time class="entry-date" datetime="%3$s" pubdate>%4$s</time></a><span class="byline"> by <span class="author vcard"><a class="url fn n" href="%5$s" title="%6$s" rel="author">%7$s</a></span></span>', '_s' ),
+		esc_url( get_permalink() ),
+		esc_attr( get_the_time() ),
+		esc_attr( get_the_date( 'c' ) ),
+		esc_html( get_the_date() ),
+		esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
+		esc_attr( sprintf( __( 'View all posts by %s', '_s' ), get_the_author() ) ),
+		esc_html( get_the_author() )
+	);
+}
+endif;
+
+/**
+ * Returns true if a blog has more than 1 category
+ *
+ * @since _s 1.0
+ */
+function _s_categorized_blog() {
+	if ( false === ( $all_the_cool_cats = get_transient( 'all_the_cool_cats' ) ) ) {
+		// Create an array of all the categories that are attached to posts
+		$all_the_cool_cats = get_categories( array(
+			'hide_empty' => 1,
+		) );
+
+		// Count the number of categories that are attached to the posts
+		$all_the_cool_cats = count( $all_the_cool_cats );
+
+		set_transient( 'all_the_cool_cats', $all_the_cool_cats );
+	}
+
+	if ( '1' != $all_the_cool_cats ) {
+		// This blog has more than 1 category so _s_categorized_blog should return true
+		return true;
+	} else {
+		// This blog has only 1 category so _s_categorized_blog should return false
+		return false;
+	}
+}
+
+/**
+ * Flush out the transients used in _s_categorized_blog
+ *
+ * @since _s 1.0
+ */
+function _s_category_transient_flusher() {
+	// Like, beat it. Dig?
+	delete_transient( 'all_the_cool_cats' );
+}
+add_action( 'edit_category', '_s_category_transient_flusher' );
+add_action( 'save_post', '_s_category_transient_flusher' );
