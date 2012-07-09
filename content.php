b01e4375e3bc61626f449b1ed78937e3c401c1b3
Kirk Wight <kwight@kwight.ca>
2012-07-09 07:59:00 -0400

    Replace pipe separators with bullets

--- content.php
+++ content.php
@@ -44,7 +44,7 @@
 				$tags_list = get_the_tag_list( '', __( ', ', '_s' ) );
 				if ( $tags_list ) :
 			?>
-			<span class="sep"> | </span>
+			<span class="sep">&nbsp&nbsp&bull;&nbsp&nbsp</span>
 			<span class="tag-links">
 				<?php printf( __( 'Tagged %1$s', '_s' ), $tags_list ); ?>
 			</span>
@@ -52,10 +52,10 @@
 		<?php endif; // End if 'post' == get_post_type() ?>
 
 		<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
-		<span class="sep"> | </span>
+		<span class="sep">&nbsp&nbsp&bull;&nbsp&nbsp</span>
 		<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', '_s' ), __( '1 Comment', '_s' ), __( '% Comments', '_s' ) ); ?></span>
 		<?php endif; ?>
 
-		<?php edit_post_link( __( 'Edit', '_s' ), '<span class="sep"> | </span><span class="edit-link">', '</span>' ); ?>
+		<?php edit_post_link( __( 'Edit', '_s' ), '<span class="sep">&nbsp&nbsp&bull;&nbsp&nbsp</span><span class="edit-link">', '</span>' ); ?>
 	</footer><!-- #entry-meta -->
 </article><!-- #post-<?php the_ID(); ?> -->
