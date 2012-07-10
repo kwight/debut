58747b09c81920a5aec347698af42d13338d8581
Kirk Wight <kwight@kwight.ca>
2012-07-09 21:55:14 -0400

    Moved useful functions from _s tweaks.php to functions.php

--- functions.php
+++ functions.php
@@ -317,4 +317,50 @@ if ( ! function_exists( 'debut_nav_menu_args' ) ) {
 	}
 
 }
-add_filter( 'wp_nav_menu_args', 'straightup_nav_menu_args' );
+add_filter( 'wp_nav_menu_args', 'debut_nav_menu_args' );
+
+
+/**
+ * Get our wp_nav_menu() fallback, wp_page_menu(), to show a home link.
+ *
+ * @since 1.0
+ */
+function debut_page_menu_args( $args ) {
+	$args['show_home'] = true;
+	return $args;
+}
+add_filter( 'wp_page_menu_args', 'debut_page_menu_args' );
+
+
+/**
+ * Adds custom classes to the array of body classes.
+ *
+ * @since 1.0
+ */
+function debut_body_classes( $classes ) {
+	// Adds a class of group-blog to blogs with more than 1 published author
+	if ( is_multi_author() ) {
+		$classes[] = 'group-blog';
+	}
+
+	return $classes;
+}
+add_filter( 'body_class', 'debut_body_classes' );
+
+
+/**
+ * Filter in a link to a content ID attribute for the next/previous image links on image attachment pages
+ *
+ * @since 1.0
+ */
+function debut_enhanced_image_navigation( $url, $id ) {
+	if ( ! is_attachment() && ! wp_attachment_is_image( $id ) )
+		return $url;
+
+	$image = get_post( $id );
+	if ( ! empty( $image->post_parent ) && $image->post_parent != $id )
+		$url .= '#main';
+
+	return $url;
+}
+add_filter( 'attachment_link', 'debut_enhanced_image_navigation', 10, 2 );
