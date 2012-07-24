eefbdb21e49b1d9576d0c00a3ad1f76e26b16e41
Kirk Wight <kwight@kwight.ca>
2012-07-23 20:18:34 -0400

    Prefix enqueued script with theme slug.

--- functions.php
+++ functions.php
@@ -89,7 +89,7 @@ function debut_scripts() {
 	}
 
 	if ( is_singular() && wp_attachment_is_image( $post->ID ) ) {
-		wp_enqueue_script( 'keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), '20120202' );
+		wp_enqueue_script( 'debut-keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), '20120202' );
 	}
 }
 add_action( 'wp_enqueue_scripts', 'debut_scripts' );
