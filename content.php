131f18633ada89dcbab9e9b30128179d59d5294f
Kirk Wight <kwight@kwight.ca>
2012-07-30 19:47:08 -0400

    On posts without thumbnails, add a class that removes the margin-left spacing

--- content.php
+++ content.php
@@ -8,26 +8,29 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php
-		if ( has_post_thumbnail() ) { ?>
+		if ( has_post_thumbnail() ) {
+			$class = null; ?>
 			<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'thumbnail' ); ?></a>
-		<?php } ?>
+		<?php
+
+		} else $class = ' no-thumb'; ?>
 
 		
-		<h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'debut' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+		<h1 class="entry-title<?php echo $class; ?>"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'debut' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
 
 		<?php if ( 'post' == get_post_type() ) : ?>
-		<div class="entry-meta">
+		<div class="entry-meta<?php echo $class; ?>">
 			<?php debut_posted_on(); ?>
 		</div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
-	<div class="entry-content">
+	<div class="entry-content<?php echo $class; ?>">
 		<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'debut' ) ); ?>
 		<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'debut' ), 'after' => '</div>' ) ); ?>
 	</div><!-- .entry-content -->
 
-	<footer class="entry-meta">
+	<footer class="entry-meta<?php echo $class; ?>">
 		<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
 			<?php
 				/* translators: used between list items, there is a space after the comma */
