1738288f07baaed45546ba9f1d4cac443731e718
Kirk Wight <kwight@kwight.ca>
2012-08-21 22:19:15 -0400

    Remove Category and Tag descriptions

--- archive.php
+++ archive.php
@@ -51,20 +51,6 @@ get_header(); ?>
 							}
 						?>
 					</h1>
-					<?php
-						if ( is_category() ) {
-							// show an optional category description
-							$category_description = category_description();
-							if ( ! empty( $category_description ) )
-								echo apply_filters( 'category_archive_meta', '<div class="taxonomy-description">' . $category_description . '</div>' );
-
-						} elseif ( is_tag() ) {
-							// show an optional tag description
-							$tag_description = tag_description();
-							if ( ! empty( $tag_description ) )
-								echo apply_filters( 'tag_archive_meta', '<div class="taxonomy-description">' . $tag_description . '</div>' );
-						}
-					?>
 				</header>
 
 				<?php rewind_posts(); ?>
