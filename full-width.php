66cbead7f31915f419e2d4d7f5bf8a8d0e1e7d00
Kirk Wight <kwight@kwight.ca>
2012-09-11 16:49:13 -0400

    Added Full Width page template

---
+++ full-width.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ *
+ * Template Name: Full Width
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package debut
+ * @since 1.0
+ */
+?>
+
+<?php get_header(); ?>
+
+		<div id="primary" class="site-content">
+			<div id="content" role="main">
+
+			<?php if ( have_posts() ) : ?>
+
+				<?php /* Start the Loop */ ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php
+						/* Include the Post-Format-specific template for the content.
+						 * If you want to overload this in a child theme then include a file
+						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+						 */
+						get_template_part( 'content', get_post_format() );
+					?>
+
+				<?php endwhile; ?>
+
+				<?php debut_content_nav( 'nav-below' ); ?>
+
+			<?php elseif ( current_user_can( 'edit_posts' ) ) : ?>
+
+				<?php get_template_part( 'no-results', 'index' ); ?>
+
+			<?php endif; ?>
+
+			</div><!-- #content -->
+		</div><!-- #primary .site-content -->
+
+<?php get_footer(); ?>
