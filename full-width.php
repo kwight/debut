06fcbda1c9b5bcc92afb0bea4ba5244ff569107c
Kirk Wight <kwight@kwight.ca>
2012-09-15 12:08:35 -0400

    Correct template to be based on page.php rather than index.php. Duh. Fixes #17.

--- full-width.php
+++ full-width.php
@@ -1,45 +1,25 @@
 <?php
 /**
- *
  * Template Name: Full Width
  *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site will use a
- * different template.
+ * This template displays the content full-width, with no sidebar.
  *
  * @package debut
  * @since 1.0
  */
-?>
 
-<?php get_header(); ?>
+get_header(); ?>
 
 		<div id="primary" class="site-content">
 			<div id="content" role="main">
 
-			<?php if ( have_posts() ) : ?>
-
-				<?php /* Start the Loop */ ?>
 				<?php while ( have_posts() ) : the_post(); ?>
 
-					<?php
-						/* Include the Post-Format-specific template for the content.
-						 * If you want to overload this in a child theme then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-						get_template_part( 'content', get_post_format() );
-					?>
+					<?php get_template_part( 'content', 'page' ); ?>
 
-				<?php endwhile; ?>
+					<?php comments_template( '', true ); ?>
 
-				<?php debut_content_nav( 'nav-below' ); ?>
-
-			<?php elseif ( current_user_can( 'edit_posts' ) ) : ?>
-
-				<?php get_template_part( 'no-results', 'index' ); ?>
-
-			<?php endif; ?>
+				<?php endwhile; // end of the loop. ?>
 
 			</div><!-- #content -->
 		</div><!-- #primary .site-content -->
