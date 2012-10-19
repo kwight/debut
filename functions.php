d580a8d5ba49ed6b8172c93bbcf948dd69dfa652
Kirk Wight <kwight@kwight.ca>
2012-10-19 12:57:50 -0400

    Added widget area to footer

--- functions.php
+++ functions.php
@@ -90,6 +90,30 @@ function debut_widgets_init() {
 		'before_title' => '<h1 class="widget-title">',
 		'after_title' => '</h1>',
 	) );
+	register_sidebar( array(
+		'name' => __( 'Footer – Left', 'debut' ),
+		'id' => 'footer-left',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget' => "</aside>",
+		'before_title' => '<h1 class="widget-title">',
+		'after_title' => '</h1>',
+	) );
+	register_sidebar( array(
+		'name' => __( 'Footer – Center', 'debut' ),
+		'id' => 'footer-center',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget' => "</aside>",
+		'before_title' => '<h1 class="widget-title">',
+		'after_title' => '</h1>',
+	) );
+	register_sidebar( array(
+		'name' => __( 'Footer – Right', 'debut' ),
+		'id' => 'footer-right',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget' => "</aside>",
+		'before_title' => '<h1 class="widget-title">',
+		'after_title' => '</h1>',
+	) );
 }
 add_action( 'widgets_init', 'debut_widgets_init' );
 
