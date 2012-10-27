24ecb2254fdbe3fb3ae616e4ee85c97df9fa22c7
Kirk Wight <kwight@kwight.ca>
2012-10-27 11:21:01 -0400

    Removed keyboard navigation. Haven't played with or tested it yet, so get it out.

--- functions.php
+++ functions.php
@@ -131,10 +131,7 @@ function debut_scripts() {
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
-
-	if ( is_singular() && wp_attachment_is_image( $post->ID ) ) {
-		wp_enqueue_script( 'debut-keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), '20120202' );
-	}
+	
 }
 add_action( 'wp_enqueue_scripts', 'debut_scripts' );
 
