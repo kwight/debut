f35f8c3365a826dfee3ad0172993314c42e852c0
Kirk Wight <kwight@kwight.ca>
2012-12-05 12:47:47 -0500

    Added 'identity' class to #masthead hgroup

--- header.php
+++ header.php
@@ -52,7 +52,7 @@
 				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><img src="<?php echo get_theme_mod( 'debut_logo' ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"></a>
 			</div>
 		<?php else : ?>
-			<hgroup>
+			<hgroup class="identity">
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 			</hgroup>
